01eb1bc5193b5a4f4e0851740eef568bf7d9349b
Vladimir Bosiak <info@vallbo.net>
2020-02-28 15:20:48 +0100

    Subtitute easter_days function
    Use algorithm from https://github.com/steinger/easter-date for calculating easter.
    This solution does not need calendar extension

--- src/Workdays/HolidaysProvider/BaseHolidaysProvider.php
+++ src/Workdays/HolidaysProvider/BaseHolidaysProvider.php
@@ -13,13 +13,27 @@ class BaseHolidaysProvider
 {
 
     /**
-     *
+     * Based on https://github.com/steinger/easter-date
      * @param int $year
      * @return DateTime
      */
     protected function getEasterSunday($year)
     {
-        return new DateTime($year . '-03-21 + ' . (easter_days($year)) . ' DAYS');
+        $J = date("Y", mktime(0, 0, 0, 1, 1, $year));
+        $K = floor($J / 100);
+        $M = 15 + floor((3 * $K + 3) / 4) - floor((8 * $K + 13) / 25);
+        $S = 2 - floor((3 * $K + 3) / 4);
+        $A = $J % 19;
+        $D = (19 * $A + $M) % 30;
+        $R = floor($D / 29) + (floor($D / 28) - floor($D / 29)) * floor($A / 11);
+        $OG = 21 + $D - $R; // March date of Easter full moon (= 14. days of the first month in the moon calendar, called Nisanu)
+        $SZ = 7 - (($J + floor($J / 4) + $S) % 7); // Date first Sunday of March
+        $OE = 7 - (($OG - $SZ) % 7);
+        $OS = $OG + $OE;
+        $easter = mktime(0, 0, 0, 3, $OS, $J);
+        $date = new DateTime();
+        $date->setTimestamp($easter);
+        return $date;
     }
 
     /**
